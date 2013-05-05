ae21253ad0c17d2615dccac8fe64257b758482b8
mke <mke@linux-0s6g.site>
2013-05-05 18:12:33 +0400

    asm1 tweet_sentiment.py

--- asm1/tweet_sentiment.py
+++ asm1/tweet_sentiment.py
@@ -5,34 +5,33 @@ import re
 def lines(fp):
     print str(len(fp.readlines()))
 
-def main():
+def load_sent_dict():
     sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    
     sent_dict = {}
     for line in sent_file:
         name, score = line.split('\t')
-	sent_dict[name] = int(score)
-    print len(sent_dict)
-    
-    
+        sent_dict[name] = int(score)
+    sent_file.close
+    return sent_dict
+
+def tweet_sentiment(tweet, sent_dict):
+    total = 0
+    for word in re.findall(r"\b[a-zA-Z]+\b",tweet['text']):
+        word = word.lower()
+        if  word in sent_dict:
+            total = total + sent_dict[word]
+    return total
+
+def main():
+    sent_dict = load_sent_dict()
+    tweet_file = open(sys.argv[2])
     for line in tweet_file:
-	tweet = json.loads(line)
-	
+        tweet = json.loads(line)
         if 'text' in line:
-    	    total = 0
-            for word in re.findall(r"\b[a-zA-Z]+\b",tweet['text']):
-                word = word.lower()
-#                print word
-                if  word in sent_dict:
-#		    print 'asdsaadadaadadadadadadadad'
-#            	    print sent_dict[word]
-            	    total = total + sent_dict[word]
-            	    
-	    print total
-    
-#    lines(sent_file)
-#    lines(tweet_file)
+            total = tweet_sentiment(tweet, sent_dict)
+            print float(total)
+
+    tweet_file.close()		
 
 if __name__ == '__main__':
     main()
