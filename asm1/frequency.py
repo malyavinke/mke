4a9b20f277dad5c064eb8bcb64d4d39ebaa9a422
mke <mke@linux-0s6g.site>
2013-05-06 22:57:39 +0400

    add frequency.py

---
+++ asm1/frequency.py
@@ -0,0 +1,31 @@
+import sys
+import json
+import re
+
+def coun_tweet_words(tweet, term_dict):
+    total = 0
+    words = re.findall(r"\b[a-zA-Z]+\b",tweet['text'])
+    for word in words:
+        word = word.lower()
+        count = 1
+        if  word in term_dict:
+            count = term_dict[word] + 1
+        term_dict[word] = count
+        total = total + 1
+    return total
+
+def main():
+    tweet_file = open(sys.argv[1])
+    total = 0
+    term_dict = {}
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if 'text' in line:
+            total = total + coun_tweet_words(tweet, term_dict)
+    for term in term_dict:
+        print term, float(term_dict[term])/total
+
+    tweet_file.close()		
+
+if __name__ == '__main__':
+    main()
